e95237a796a8ac075a0864446d112f6159a5a2c6
DESKTOP-8VN5A5S\SEO <seoyoungjun88@gmail.com>
2021-03-24 10:29:35 +0900

    회원가입 기능

--- common/views.py
+++ common/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField(label="Email")
+
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
+def signup(request):
+    """
+    계정생성
+    """
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserForm()
+
+    return render(request, 'common/signup.html', {'form': form})
