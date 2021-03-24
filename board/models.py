8e39595d28cc38b706f14719203af4b1a7fcea2f
DESKTOP-8VN5A5S\SEO <seoyoungjun88@gmail.com>
2021-03-25 08:54:29 +0900

    계정명 추가

--- board/models.py
+++ board/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django import forms
 
@@ -12,6 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     create_date = models.DateTimeField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -37,4 +39,5 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     comment = models.TextField()
     create_date = models.DateTimeField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- board/views.py
+++ board/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
@@ -11,7 +12,6 @@ from django.core.paginator import Paginator
 def index(request):
 
     page = request.GET.get('page', '1')
-
     post_list = Post.objects.order_by('-create_date')
 
     paginator = Paginator(post_list, 10)  # 페이지당 10개씩 보여주기
@@ -39,22 +39,25 @@ def detail(request, post_id):
 
 
 # 댓글 작성
+@login_required(login_url='common:login')
 def create_comment(request, post_id):
     # post = Post.objects.get(id=post_id)
     # 404 에러페이지 처리
     post = get_object_or_404(Post, pk=post_id)
-    post.comment_set.create(comment=request.POST.get('comment'), create_date=timezone.now())
+    post.comment_set.create(comment=request.POST.get('comment'), writer=request.user, create_date=timezone.now())
 
     return redirect('post:detail', post_id=post.id)
 
 
 # 게시글 작성
+@login_required(login_url='common:login')
 def create_post(request):
 
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
+            post.writer = request.user
             post.create_date = timezone.now()
             post.save()
             return redirect('post:index')
